1fe6fd5b875da874b822e0e5485324a710edcf01
Jordi Petit <jordi.petit@gmail.com>
2016-07-04 11:00:39 +0200

    Polished version.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-This is a test.
+Simple functions to read input from Python for problems in Jutge.org.
+
+This package exports a `read` function that returns the next token of the
+input. The type of the token must be given as a parameter: `read(int)`,
+`read(float)`, `read(str)`... In the event no more tokens are available,
+`read` returns `None`.
+
+Sample program to compute the sum of a sequence of integers:
+
+```python
+from jutge import read
+
+s = 0
+x = read(int)
+while x is not None:
+    s += x
+    x = read(int)
+print(s)
+```
--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'jutge',
   packages = ['jutge'],
-  version = '0.1',
-  description = 'A random test lib',
+  version = '1.0',
+  description = 'Simple functions to read input from Python for problems in Jutge.org.',
   author = 'Jordi Petit',
   author_email = 'jpetit@cs.upc.edu',
   url = 'https://github.com/jutge-org/jutge-python',
-  download_url = 'https://github.com/jutge-org/jutge-python/tarball/0.1', # I'll explain this in a second
+  download_url = 'https://github.com/jutge-org/jutge-python/tarball/1.0',
   keywords = ['jutge', 'jutge.org'],
   classifiers = [],
 )
