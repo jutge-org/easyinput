7d7aa17a7a467b81f47a8385e0304f07262c0851
Jordi Petit <jordi.petit@gmail.com>
2016-07-04 11:53:56 +0200

    Added some help to remember the upload to PyPI steps.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'jutge',
   packages = ['jutge'],
-  version = '1.1',
+  version = '1.2',
   description = 'Simple functions to read input from Python for problems in Jutge.org.',
   author = 'Jordi Petit',
   author_email = 'jpetit@cs.upc.edu',
@@ -12,3 +12,15 @@ setup(
   keywords = ['jutge', 'jutge.org'],
   classifiers = [],
 )
+
+
+# Steps to distribute new version:
+#
+# Increment version in setup.py
+# git commit -a
+# git push
+# git tag 1.12345 -m "Adds a tag so that we can put this on PyPI.".
+# git push --tags origin master
+# python setup.py sdist upload
+#
+# More doc: http://peterdowns.com/posts/first-time-with-pypi.html
