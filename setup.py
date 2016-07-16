def25e5d1af0ffa1deb7569429f572c9f1e6aa1c
Jordi Petit <jordi.petit@gmail.com>
2016-07-16 22:30:46 +0200

    Create new release 1.4.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'jutge',
   packages = ['jutge'],
-  version = '1.3',
+  version = '1.4',
   description = 'Simple functions to read input from Python for problems in Jutge.org.',
   author = 'Jordi Petit',
   author_email = 'jpetit@cs.upc.edu',
   url = 'https://github.com/jutge-org/jutge-python',
-  download_url = 'https://github.com/jutge-org/jutge-python/tarball/1.0',
+  download_url = 'https://github.com/jutge-org/jutge-python/tarball/1.4',
   keywords = ['jutge', 'jutge.org'],
   classifiers = [],
 )
@@ -16,7 +16,7 @@ setup(
 
 # Steps to distribute new version:
 #
-# Increment version in setup.py
+# Increment version id in "version" and "download_url"
 # git commit -a
 # git push
 # git tag 1.12345 -m "Adds a tag so that we can put this on PyPI.".
