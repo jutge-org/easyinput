71c77861fbfecc31785220e9631715ee83b4e4ea
Jordi Petit <jordi.petit@gmail.com>
2016-07-16 17:07:28 +0200

    Improved test script.

--- test.sh
+++ test.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
 
+fail=0
+
 cd test
-
-for d in *.tst ; do
-
+for d in *.tst
+do
     cd $d
     cp -r ../../jutge .
-
-    for t in *.inp ; do
-
-        echo $d/$t
-
+    for t in *.inp
+    do
         t=`basename $t .inp`
         python test.py <$t.inp >$t.out
-        diff $t.out $t.cor || exit 1
-
+        cmp --silent $t.out $t.cor
+        if [ $? -ne 0 ]
+        then
+            fail=1
+            echo $d/$t fail
+        else
+            echo $d/$t ok
+        fi
     done
-
     cd ..
-
 done
+
+exit $fail
