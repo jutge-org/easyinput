66364e8bdbc6b514983c56a3634a52f374cddbe3
Jordi Petit <jordi.petit@gmail.com>
2016-07-16 16:52:58 +0200

    Generalized the way to write tests and created a test that evidences a bug.

--- test.sh
+++ test.sh
@@ -2,6 +2,22 @@
 
 
 cd test
-rm -rf jutge
-cp -r ../jutge .
-python sum.py <sum-test-1.inp >sum-test-1.out ; diff sum-test-1.out sum-test-1.cor
+
+for d in *.tst ; do
+
+    cd $d
+    cp -r ../../jutge .
+
+    for t in *.inp ; do
+
+        echo $d/$t
+
+        t=`basename $t .inp`
+        python test.py <$t.inp >$t.out
+        diff $t.out $t.cor
+
+    done
+
+    cd ..
+
+done
