f102d192701158a9969d97114a81f2d96685abf3
Jordi Petit <jordi.petit@gmail.com>
2016-07-22 00:34:36 +0200

    Add setup information for PyPi

--- setup.py
+++ setup.py
@@ -3,14 +3,29 @@ from distutils.core import setup
 setup(
   name = 'jutge',
   packages = ['jutge'],
-  version = '1.4',
+  version = '1.5',
   description = 'Simple functions to read input from Python for problems in Jutge.org.',
   author = 'Jordi Petit',
   author_email = 'jpetit@cs.upc.edu',
   url = 'https://github.com/jutge-org/jutge-python',
-  download_url = 'https://github.com/jutge-org/jutge-python/tarball/1.4',
-  keywords = ['jutge', 'jutge.org'],
+  download_url = 'https://github.com/jutge-org/jutge-python/tarball/1.5',
+  keywords = ['jutge', 'jutge.org', 'education'],
   classifiers = [],
+  license='Apache',
+  classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Education',
+    ],
 )
 
 
@@ -19,7 +34,7 @@ setup(
 # Increment version id in "version" and "download_url"
 # git commit -a
 # git push
-# git tag 1.12345 -m "Adds a tag so that we can put this on PyPI.".
+# git tag 1.12345 -m "Release 1.12345"
 # git push --tags origin master
 # python setup.py sdist upload
 #
