643ffdc3ee97db2deb19f34a8e678ad3941203bb
Jordi Petit <jordi.petit@gmail.com>
2016-10-04 15:48:27 +0200

    Apply PEP8

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from distutils.core import setup
 
 setup(
-  name = 'jutge',
-  packages = ['jutge'],
-  version = '1.6',
-  description = 'Simple functions to read input from Python for problems in Jutge.org.',
-  author = 'Jordi Petit',
-  author_email = 'jpetit@cs.upc.edu',
-  url = 'https://github.com/jutge-org/jutge-python',
-  download_url = 'https://github.com/jutge-org/jutge-python/tarball/1.5',
-  keywords = ['jutge', 'jutge.org', 'education'],
-  license='Apache',
-  classifiers=[
+    name='jutge',
+    packages=['jutge'],
+    version='1.7',
+    description='Simple functions to read input from Python for problems in Jutge.org.',
+    author='Jordi Petit',
+    author_email='jpetit@cs.upc.edu',
+    url='https://github.com/jutge-org/jutge-python',
+    download_url='https://github.com/jutge-org/jutge-python/tarball/1.7',
+    keywords=['jutge', 'jutge.org', 'education'],
+    license='Apache',
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Intended Audience :: Education',
