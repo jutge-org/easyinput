78d20400d7e819316f6bb3d3dbe7a2d74c86d8ae
Paolo Lammens <lammenspaolo@gmail.com>
2018-12-13 16:21:23 +0100

    test: Update and add Python version test

--- jutge/__init__.py
+++ jutge/__init__.py
@@ -4,6 +4,7 @@ see https://github.com/jutge-org/jutge-python
 """
 
 import sys
+from builtins import int  # for Python2 compatibility
 
 __all__ = ['read', 'keep_reading']  # Specify what to import with *
 version = "1.8"  # current version
