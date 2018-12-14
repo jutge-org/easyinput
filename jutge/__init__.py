15b0bba8a9665c2e164d379da04e6bb4c4353e13
Paolo Lammens <lammenspaolo@gmail.com>
2018-12-14 18:04:17 +0100

    test: Add more tests

--- jutge/__init__.py
+++ jutge/__init__.py
@@ -120,7 +120,7 @@ def read(*types, **kwargs):
             tokens.typ = typ  # Set fixed type for efficiency
             return (next(tokens) for _ in range(amount))
     else:  # Return generator of type-heterogenous values
-        return (tokens.nexttoken(typ) for _ in range(amount) for typ in types )
+        return (tokens.nexttoken(typ) for _ in range(amount) for typ in types)
 
 
 @kwd_only(file=__StdIn, amount=1)  # Python 2 compatibility
--- test/test_keep_reading.py
+++ test/test_keep_reading.py
@@ -32,3 +32,33 @@ def test_4():
     )
     input_gen = jutge.keep_reading(float, file=source)
     assert all(next(input_gen) == val for val in expected)
+    
+    
+def test_5():
+    source = open('./test/text/source4.txt', 'r')
+    expected = (
+        ("Avatar", 2008),
+        ("Thor", 2010),
+        ("The_Great_Gatsby", 1956),
+        ("GATTACA", 1999),
+        ("Blade_Runner", 1981),
+        ("Cars", 2005),
+        ("A_Really_Long_Title", 2050),
+        ("Monty_Python", 1010)
+    )
+    input_gen = jutge.keep_reading(str, int, file=source)
+    assert all(tuple(next(input_gen)) == val for val in expected)
+    
+    
+def test_6():
+    source = open('./test/text/source3.txt', 'r')
+    expected = (tuple(range(1 + 3*d, 4 + 3*d)) for d in range(4))
+    input_gen = jutge.keep_reading(int, file=source, amount=3)
+    assert all(tuple(next(input_gen)) == tuple(val) for val in expected)
+
+
+def test_7():
+    source = open('./test/text/source3.txt', 'r')
+    expected = (tuple(range(1 + 5*d, 6 + 5*d)) for d in range(2))
+    input_gen = jutge.keep_reading(int, file=source, amount=5)
+    assert all(tuple(next(input_gen)) == tuple(val) for val in expected)
