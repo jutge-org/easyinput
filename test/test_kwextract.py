06c15a252465564788e3afcee518559d5f196964
Paolo Lammens <lammenspaolo@gmail.com>
2018-12-14 12:26:57 +0100

    test: Add new test for kwarg extraction

---
+++ test/test_kwextract.py
@@ -0,0 +1,11 @@
+from jutge import read
+
+
+def test_bad_kwarg():
+    raised = False
+    try:
+        read(nonexistant_kwarg=42)
+    except NameError:
+        raised = True
+
+    assert raised
