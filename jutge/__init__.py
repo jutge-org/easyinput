e5b38af3843668c49d2385801e789ce5b18c9990
Paolo Lammens <lammenspaolo@gmail.com>
2018-12-14 23:59:03 +0100

    fix: Python 2 `input` compatibility

--- jutge/__init__.py
+++ jutge/__init__.py
@@ -4,7 +4,7 @@ see https://github.com/jutge-org/jutge-python
 """
 
 import sys
-from future.builtins import int  # for Python2 compatibility
+from future.builtins import int, input  # for Python2 compatibility
 from jutge.utils import kwd_only, identity
 
 __all__ = ['read', 'keep_reading']  # Specify what to import with *
