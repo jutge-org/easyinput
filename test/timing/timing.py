a64e7f455f2d9da085fb25b8764c19294ff5baee
Paolo Lammens <lammenspaolo@gmail.com>
2018-12-22 18:06:40 +0100

    test: Add stuff

--- test/timing/timing.py
+++ test/timing/timing.py
@@ -10,26 +10,26 @@ colorama.init()
 
 
 def time_new_1():
-    for _ in jutge.keep_reading(file=open('../text/long.txt', 'r')):
+    for _ in jutge.keep_reading(file=open('../text/long.txt')):
         pass
 
 
 def time_old_1():
-    file = open('../text/long.txt', 'r')
+    file = open('../text/long.txt')
     word = jutge_old.read(file=file)
     while word is not None:
         word = jutge_old.read(file=file)
 
 
 def time_new_2():
-    with open('../text/source5.txt', 'r') as file:
+    with open('../text/source5.txt') as file:
         nums = []
         for line in jutge.keep_reading(int, file=file, amount=100, astuple=True):
             nums.append(line)
 
 
 def time_old_2():
-    with open('../text/source5.txt', 'r') as file:
+    with open('../text/source5.txt') as file:
         tup = tuple(jutge_old.read(*(int,) * 100, file=file))
         nums = [tup]
         while tup[-1] is not None:
@@ -38,13 +38,23 @@ def time_old_2():
 
 
 def time_new_3():
-    with open('../text/source5.txt', 'r') as file:
+    with open('../text/source5.txt') as file:
         sum(jutge.read(int, amount=1000, file=file))
 
 
 def time_old_3():
-    with open('../text/source5.txt', 'r') as file:
+    with open('../text/source5.txt') as file:
         sum(jutge_old.read(*(int,) * 1000, file=file))
+
+
+def time_new_4():
+    with open('../text/long.txt') as file:
+        jutge.read(chr, amount=1000, file=file, astuple=True)
+
+
+def time_old_4():
+    with open('../text/long.txt') as file:
+        jutge_old.read(*(chr,) * 1000, file=file)
 
 
 class Job:
@@ -58,9 +68,10 @@ repeats = 10  # (constant) Number of repetitions per job
 
 if __name__ == '__main__':
     comparisons = (
-        Job("Sum over int", 3, 400),
-        Job("Read/iterate sequences of int", 2, 40),
-        Job("Read long file", 1, 1)
+        Job("Sum over int", 3, 300),
+        Job("Read/iterate sequences of int", 2, 35),
+        Job("Read chars", 4, 500),
+        Job("Read long file", 1, 1),
     )
     for comp in comparisons:
         for v in "old", "new":
@@ -73,6 +84,6 @@ if __name__ == '__main__':
             for i in range(repeats):
                 times.append(t.timeit(number=comp.reps))
                 print(" {:.4f}".format(times[-1]), end='')
-            print(colored("\n\t\tBEST: {:.4f}".format(min(times)), color='red', attrs=('bold',)))
+            print(colored("\n\t\tBEST: {:.4f} s".format(min(times)), color='red', attrs=('bold',)))
 
         print('\n')
