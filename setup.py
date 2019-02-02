8884165ac0741a0aeadd4547546504e4a9529762
Paolo Lammens <lammenspaolo@gmail.com>
2019-02-02 17:51:22 +0100

    chore: requirements

--- setup.py
+++ setup.py
@@ -3,35 +3,39 @@ from distutils.core import setup
 import jutge
 
 
-setup(
-    name='jutge',
-    packages=['jutge'],
-    version=jutge.version,
-    description='Simple functions to read input from Python for problems in Jutge.org.',
-    author='Jordi Petit',
-    author_email='jpetit@cs.upc.edu',
-    url='https://github.com/jutge-org/jutge-python',
-    download_url='https://github.com/jutge-org/jutge-python/tarball/{}'.format(jutge.version),
-    keywords=['jutge', 'jutge.org', 'education', 'input'],
-    license='Apache',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Education',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Education',
-    ],
-    test_suite=['pytest-runner'],
-    tests_require=['pytest']
-)
+with open('requirements.txt') as requirements:
+    setup(
+        name='jutge',
+        packages=['jutge'],
+        install_requires=requirements.read(),
+        version=jutge.version,
+        description='Simple functions to read input from Python for problems in Jutge.org.',
+        author='Jordi Petit',
+        author_email='jpetit@cs.upc.edu',
+        url='https://github.com/jutge-org/jutge-python',
+        download_url='https://github.com/jutge-org/jutge-python/tarball/{}'.format(jutge.version),
+
+        keywords=['jutge', 'jutge.org', 'education', 'input'],
+        license='Apache',
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Console',
+            'Intended Audience :: Education',
+            'License :: OSI Approved :: Apache Software License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.7',
+            'Topic :: Education',
+        ],
+
+        test_suite=['pytest-runner'],
+        tests_require=['pytest']
+    )
 
 
 # Steps to distribute new version:
